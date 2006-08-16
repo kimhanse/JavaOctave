b3f4efb6dbfad4399b2eafcd36b8b08314544344
kim <>
2006-08-16 13:27:47 +0000

    Silence warning about not knowing 2.9.7. Put svnversion in MANIFEST.MF.

--- src/dk/ange/octave/setup.m
+++ src/dk/ange/octave/setup.m
@@ -2,7 +2,7 @@
 if (strcmp(octave_config_info.version, "2.9.5"))
   crash_dumps_octave_core = 0;
   sigterm_dumps_octave_core = 0;
-elseif (strcmp(octave_config_info.version, "2.9.6"))
+elseif (strcmp(octave_config_info.version, "2.9.6") || strcmp(octave_config_info.version, "2.9.7"))
   crash_dumps_octave_core(0);
   sigterm_dumps_octave_core(0);
 else
